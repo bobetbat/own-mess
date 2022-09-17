58eb9d05945e46dca832e1f95bce713c4b5a36f9
bobetbat <b.bobetayev.97@gmail.com>
2022-09-17 12:41:55 +0200

    init: added base UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
+import { Container, Box } from '@mui/material';
+
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './components';
+import Layout from './components/Layout';
+import Block from './components/Block';
+import Menu from './components/Menu';
 
-function App() {
+export interface Page {
+  title: string;
+  content: React.ReactNode
+}
+
+const pages:Page[] = [
+  {
+    title: 'About Product',
+    content: 'About',
+  },
+  {
+    title: 'About us',
+    content: 'About',
+  },
+  {
+    title: 'Buy here',
+    content: 'Product + Payment flow',
+  },
+  {
+    title: 'Contact us',
+    content: 'contact form',
+  },
+]
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Layout>
+        <Header pages={pages} />
+        <Menu pages={pages} />
+      </Layout>
     </div>
   );
 }
